296569d7400a61398bf7b2eddcdbed0d5959991d
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-19 18:17:22 -0700

    Add an event listener, a change event, a callback function, and conditional statements to script.js so that when the user changes the mood from the drop-down list, the page's background is changed to correspond with the selected mood

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// select the drop-down list and the body of the index.html page
+var moods = document.querySelector("#moods");
+var body = document.querySelector("body");
 
+// add an event listener for a change event that activates when the moods drop-down list is changed
+moods.addEventListener("change", function (e) {
+  // target the value of the drop-down list
+  var mood = e.target.value;
+  if (mood === "happy") {
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+    body.classList.add("happy");
+  } else if (mood === "sad") {
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+    body.classList.add("sad");
+  } else if (mood === "passionate") {
+    body.classList.remove("sad");
+    body.classList.remove("happy");
+    body.classList.add("passionate");
+  }
+});
